19de6b989dd119450f8c5f2ced5fef3c66ec7670
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2024-05-06 12:29:46 +0200

    Make sound originate from flushed entity

--- control.lua
+++ control.lua
@@ -1,5 +1,8 @@
 script.on_event(defines.events.on_player_flushed_fluid,
     function(event)
-        game.play_sound({path = "sfx_flush"})
+        game.play_sound({
+            path = "sfx_flush",
+            position = event.entity.position,
+        })
     end
 )
