0f96ef3447b9ac2c81a238cd420e4718bab22df8
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2024-05-06 11:23:42 +0200

    Initial commit

---
+++ control.lua
@@ -0,0 +1,5 @@
+script.on_event(defines.events.on_player_flushed_fluid,
+    function(event)
+        game.play_sound({path = "sfx_flush"})
+    end
+)
---
+++ data.lua
@@ -0,0 +1,6 @@
+data:extend({{
+    type="sound",
+    name="sfx_flush",
+    filename="__flush__/sound/flush.ogg",
+    volume=0.7
+}})
